a9b7c1d47a23158dc5834baa964d7bdf0ffbaca7
PravalikaReddygithub <pravalikadevudi@gmail.com>
2024-05-10 09:49:43 +0000

    message

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      className="img-container"
+      alt="about"
+    />
+    <h1 className="heading">About</h1>
+    <p className="paragraph">I love to create!I am a frontend web developer</p>
+  </div>
+)
+
+export default About
